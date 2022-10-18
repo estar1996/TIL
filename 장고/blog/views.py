6f343cbce6f5e644798de8f0b1dfcae79833d33f
min-ju-Jo <whakfn99@gmail.com>
2022-10-18 17:52:10 +0900

    1018

---
+++ 장고/blog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    posts = Post.objects.all().order_by('-pk')
+    return render(
+        request,
+        'blog/index.html',
+        {
+            'posts':posts,
+        }
+    )
