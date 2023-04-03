e9acd23c03445997f67f5094286b5d0187b14177
estar1996 <estar19961996@gmail.com>
2023-04-03 17:42:47 +0900

    0403

--- algo/백준/13460 구슬 탈출 2/main.py
+++ algo/백준/13460 구슬 탈출 2/main.py
@@ -5,12 +5,30 @@ N,M = map(int,input().split())
 
 arr = []
 for _ in range(M):
-    l = list(map(int,input().split()))
+    l = list(input())
+    for i in range(N):
+        if l[i] == 'B':
+            l[i] = 1
+        elif l[i] == 'R':
+            l[i] = 2
+        elif l[i] == "O":
+            l[i] = 3
+        elif l[i] == '.':
+            l[i] = 0
+        else:
+            l[i] = 5
     arr.append(l)
+dx = [1,0,-1,0]
+dy = [0,1,0,-1]
 
-for i in range(N):
-    for j in range(M):
-        if arr[i][j] !='B'
+def R(x,y):
+    for i in range(1,N):
+        if arr[x+i][y] == 0:
+            arr[x+i][y] = arr[x][y]
+            arr[x][y] = 0
+        else:
+            arr
 
 
-def dfs(i,j):
+
+print(arr)
