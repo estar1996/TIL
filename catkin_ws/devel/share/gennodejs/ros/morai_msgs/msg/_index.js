023d9777a1653e8f12dc3893dbcebba27fb319f3
estar1996 <estar1996@naver.com>
2023-04-05 12:06:23 +0900

    morai_pjt

---
+++ catkin_ws/devel/share/gennodejs/ros/morai_msgs/msg/_index.js
@@ -0,0 +1,110 @@
+
+"use strict";
+
+let RadarDetections = require('./RadarDetections.js');
+let VehicleSpecIndex = require('./VehicleSpecIndex.js');
+let Lamps = require('./Lamps.js');
+let ERP42Info = require('./ERP42Info.js');
+let GhostMessage = require('./GhostMessage.js');
+let IntersectionStatus = require('./IntersectionStatus.js');
+let MoraiTLIndex = require('./MoraiTLIndex.js');
+let SyncModeCmdResponse = require('./SyncModeCmdResponse.js');
+let IntscnTL = require('./IntscnTL.js');
+let SyncModeScenarioLoad = require('./SyncModeScenarioLoad.js');
+let SyncModeRemoveObject = require('./SyncModeRemoveObject.js');
+let SyncModeInfo = require('./SyncModeInfo.js');
+let SyncModeAddObject = require('./SyncModeAddObject.js');
+let MapSpec = require('./MapSpec.js');
+let ObjectStatusList = require('./ObjectStatusList.js');
+let ReplayInfo = require('./ReplayInfo.js');
+let ScenarioLoad = require('./ScenarioLoad.js');
+let PRCtrlCmd = require('./PRCtrlCmd.js');
+let EgoVehicleStatus = require('./EgoVehicleStatus.js');
+let VehicleCollisionData = require('./VehicleCollisionData.js');
+let DdCtrlCmd = require('./DdCtrlCmd.js');
+let SensorPosControl = require('./SensorPosControl.js');
+let MoraiSimProcStatus = require('./MoraiSimProcStatus.js');
+let MoraiSimProcHandle = require('./MoraiSimProcHandle.js');
+let MultiEgoSetting = require('./MultiEgoSetting.js');
+let IntersectionControl = require('./IntersectionControl.js');
+let GPSMessage = require('./GPSMessage.js');
+let SetTrafficLight = require('./SetTrafficLight.js');
+let NpcGhostCmd = require('./NpcGhostCmd.js');
+let MapSpecIndex = require('./MapSpecIndex.js');
+let EventInfo = require('./EventInfo.js');
+let WaitForTickResponse = require('./WaitForTickResponse.js');
+let RadarDetection = require('./RadarDetection.js');
+let SyncModeCtrlCmd = require('./SyncModeCtrlCmd.js');
+let PRStatus = require('./PRStatus.js');
+let CtrlCmd = require('./CtrlCmd.js');
+let SyncModeSetGear = require('./SyncModeSetGear.js');
+let SyncModeCmd = require('./SyncModeCmd.js');
+let MoraiTLInfo = require('./MoraiTLInfo.js');
+let PREvent = require('./PREvent.js');
+let MoraiSrvResponse = require('./MoraiSrvResponse.js');
+let CollisionData = require('./CollisionData.js');
+let ObjectStatus = require('./ObjectStatus.js');
+let SyncModeResultResponse = require('./SyncModeResultResponse.js');
+let VehicleSpec = require('./VehicleSpec.js');
+let TrafficLight = require('./TrafficLight.js');
+let GetTrafficLightStatus = require('./GetTrafficLightStatus.js');
+let WaitForTick = require('./WaitForTick.js');
+let NpcGhostInfo = require('./NpcGhostInfo.js');
+let VehicleCollision = require('./VehicleCollision.js');
+let SaveSensorData = require('./SaveSensorData.js');
+let EgoDdVehicleStatus = require('./EgoDdVehicleStatus.js');
+
+module.exports = {
+  RadarDetections: RadarDetections,
+  VehicleSpecIndex: VehicleSpecIndex,
+  Lamps: Lamps,
+  ERP42Info: ERP42Info,
+  GhostMessage: GhostMessage,
+  IntersectionStatus: IntersectionStatus,
+  MoraiTLIndex: MoraiTLIndex,
+  SyncModeCmdResponse: SyncModeCmdResponse,
+  IntscnTL: IntscnTL,
+  SyncModeScenarioLoad: SyncModeScenarioLoad,
+  SyncModeRemoveObject: SyncModeRemoveObject,
+  SyncModeInfo: SyncModeInfo,
+  SyncModeAddObject: SyncModeAddObject,
+  MapSpec: MapSpec,
+  ObjectStatusList: ObjectStatusList,
+  ReplayInfo: ReplayInfo,
+  ScenarioLoad: ScenarioLoad,
+  PRCtrlCmd: PRCtrlCmd,
+  EgoVehicleStatus: EgoVehicleStatus,
+  VehicleCollisionData: VehicleCollisionData,
+  DdCtrlCmd: DdCtrlCmd,
+  SensorPosControl: SensorPosControl,
+  MoraiSimProcStatus: MoraiSimProcStatus,
+  MoraiSimProcHandle: MoraiSimProcHandle,
+  MultiEgoSetting: MultiEgoSetting,
+  IntersectionControl: IntersectionControl,
+  GPSMessage: GPSMessage,
+  SetTrafficLight: SetTrafficLight,
+  NpcGhostCmd: NpcGhostCmd,
+  MapSpecIndex: MapSpecIndex,
+  EventInfo: EventInfo,
+  WaitForTickResponse: WaitForTickResponse,
+  RadarDetection: RadarDetection,
+  SyncModeCtrlCmd: SyncModeCtrlCmd,
+  PRStatus: PRStatus,
+  CtrlCmd: CtrlCmd,
+  SyncModeSetGear: SyncModeSetGear,
+  SyncModeCmd: SyncModeCmd,
+  MoraiTLInfo: MoraiTLInfo,
+  PREvent: PREvent,
+  MoraiSrvResponse: MoraiSrvResponse,
+  CollisionData: CollisionData,
+  ObjectStatus: ObjectStatus,
+  SyncModeResultResponse: SyncModeResultResponse,
+  VehicleSpec: VehicleSpec,
+  TrafficLight: TrafficLight,
+  GetTrafficLightStatus: GetTrafficLightStatus,
+  WaitForTick: WaitForTick,
+  NpcGhostInfo: NpcGhostInfo,
+  VehicleCollision: VehicleCollision,
+  SaveSensorData: SaveSensorData,
+  EgoDdVehicleStatus: EgoDdVehicleStatus,
+};
